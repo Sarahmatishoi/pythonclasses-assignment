6da163f2c71923d707e2b51f04dce9e60be51039
sarahmatishoi <sarahmatishoi@gmail.com>
2021-06-17 10:41:39 +0300

    second commit

---
+++ pyquiz/python_test.py
@@ -0,0 +1,53 @@
+x=[100,110,120,130,140,150,]
+p=[]
+for y in x:
+    p.append(y*5)
+    print(p)
+
+def divisisble_by_three(n):
+    for x in n:
+        if x%3==0:
+            return x
+
+
+def numbers(): 
+    p=[]
+    sublist=[[1,2],[3,4],[5,6]]
+    
+    for list in sublist:
+        p.extend(list)
+    print(p)
+numbers()
+# def smallest():
+#      a=[2,5,4,6,8,4]
+#      for s in a:
+#          return s.min()
+# smallest()
+def  letter():
+    r= ['a','b','a','e','d','b','c','e','f','g','h']
+    return r.sort()
+    
+letter()
+def divisible_by_seven ():
+    h=range(100,200)
+    for g in h:
+        if g%7==0:
+            print(g)
+divisible_by_seven()
+def greet():
+   
+    students=[{"name":"Eunice","age":19},{"name":"Agnes","age":21},{"name":"Teresa","age":18},{"name":"Asha","age":22}]
+    
+    return f"Hello {students.name}, you were born in {2021-students.age}"
+greet()
+class Rectangle():
+    def __init__(self,width,length):
+        self.width=width
+        self.length=length
+    def area():
+        return 6*5
+    def perimeter():
+        return 8*6
+   
+        
+    
